8512a1e3863e67116895cf3664b9d3b48e64e9a4
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2026-01-07 22:15:29 +0530

    Create AppDatabase.java

---
+++ app/src/main/java/com/example/bcapp/AppDatabase.java
@@ -0,0 +1,26 @@
+package com.example.bcapp;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {BcEntity.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract BcDao bcDao();
+
+    private static volatile AppDatabase INSTANCE;
+
+    public static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "bc_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
